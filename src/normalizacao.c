f055d894559847e5b9e993de689dfcfb4ea68726
Ezequiel - OSX <ezequiel.ifsp@gmail.com>
2014-04-04 20:07:28 -0300

    normalizacao para preto e branco utilizando media simples. normalizacao.c e normalizacao.h

---
+++ src/normalizacao.c
@@ -0,0 +1,18 @@
+#include "normalizacao.h"
+
+int normalizacao_preto_e_branco(unsigned char ***matriz, int altura, int largura){
+
+	unsigned char preto_e_branco;
+
+    for (int i = 0; i < altura; i++)
+    {
+        for (int j = 0; j < largura; j++)
+        {
+            preto_e_branco = (matriz[i][j][0] + matriz[i][j][1] + matriz[i][j][2]) / 3;
+            matriz[i][j][0] = preto_e_branco;
+            matriz[i][j][1] = preto_e_branco;
+            matriz[i][j][2] = preto_e_branco;
+        }
+    }
+    return;
+}
