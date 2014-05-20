060ad2c8fc8f25904e67cfbf3500ddabebc1b7dc
Gabriel <colecionador.gabriel@gmail.com>
2014-05-19 21:47:01 -0300

    Adicionada funcao de jogabilidade

---
+++ src/jogabilidade.c
@@ -0,0 +1,34 @@
+#include "jogabilidade.h"
+#DEFINE PEDRA 2
+#DEFINE TESOURA 1
+#DEFINE PAPEL 0
+#DEFINE EMPATE 0
+#DEFINE JOGADOR_2 2
+#DEFINE JOGADOR_1 1
+
+int ganhador_rodada(int jogador_1, int jogador_2){
+	if(jogador_1 == jogador_2){
+		return EMPATE;
+	}
+	
+	if(jogador_1 == PEDRA && jogador_2 == PAPEL){
+		return JOGADOR_2;
+	}
+	if(jogador_2 == PEDRA && jogador_1 == PAPEL){
+		return JOGADOR_1;
+	}
+	
+	if(jogador_1 > jogador_2) {
+		return JOGADOR_1;
+	}
+	else 
+		return JOGADOR_2;
+}
+
+int fim_de_jogo(int pontos_jogador_1, int pontos_jogador_2, int melhor_jogo){
+	//Verifica se jogadas sao impares.
+	if((melhor_jogo % 2) > 0)
+		return (pontos_jogador_1 > (melhor_jogo/2) || pontos_jogador_2 > (melhor_jogo/2));
+	else
+		return (pontos_jogador_1 > (melhor_jogo/2) || pontos_jogador_2 > (melhor_jogo/2) || (pontos_jogador_1 + pontos_jogador_2) == melhor_jogo);
+}
