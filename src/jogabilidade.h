272058f8a9c8f4d9f94dcccfd9f92f400197ac69
Gabriel <colecionador.gabriel@gmail.com>
2014-05-19 21:48:01 -0300

    Atualizada declaracao de funcoes em jogabilidade

--- src/jogabilidade.h
+++ src/jogabilidade.h
@@ -1,8 +1,8 @@
 #ifndef JOGABILIDADE_H
 #define JOGABILIDADE_H
 
-int ganhador_rodada();
+int ganhador_rodada(int jogador_1, int jogador_2);
 
-int fim_de_jogo();
+int fim_de_jogo(int pontos_jogador_1, int pontos_jogador_2, int melhor_jogo);
 
 #endif
