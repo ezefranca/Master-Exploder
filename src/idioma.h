69ba0a2b3e9d85f9cf50c6e00138637532bef1ab
Gabriel <colecionador.gabriel@gmail.com>
2014-05-21 01:10:07 -0300

    Atualizado include de idioma

--- src/idioma.h
+++ src/idioma.h
@@ -11,13 +11,10 @@ typedef struct {
 	elemento_string **hash;
 } IDIOMA;
  
-IDIOMA *idioma;
-
 void carregar_idioma(const char *arquivo);
 
 char *pegar_idioma(const char *key, IDIOMA *idioma);
 
 void destroi_idioma(IDIOMA *language);
-
 
 #endif
