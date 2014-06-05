cc3f668a7e3913fd98d9df7e8eb04436db681026
Gabriel <colecionador.gabriel@gmail.com>
2014-06-05 16:45:24 -0300

    Update jogabilidade

--- src/jogabilidade.c
+++ src/jogabilidade.c
@@ -1,4 +1,6 @@
 #include "jogabilidade.h"
+#include <time.h>
+
 
 int ganhador_rodada(int jogador_1, int jogador_2){
 	if(jogador_1 == jogador_2)
@@ -32,6 +34,17 @@ int fazer_jogada_melhor(int jogador){
 	if(jogador == PEDRA)
 		return PAPEL;
 	return jogador + 1;
+}
+
+int fazer_jogada_pior(int jogador) {
+	if(jogador == PAPEL)
+		return PEDRA;
+	return jogador - 1;
+}
+
+int rand_boss() {
+	int randomic = srand(abs(time(NULL))); 
+	
 }
 
 int fim_de_jogo(int pontos_jogador_1, int pontos_jogador_2, int melhor_jogo){
--- src/jogabilidade.h
+++ src/jogabilidade.h
@@ -9,6 +9,8 @@ int ganhador_jogo(int jogador_1, int jogador_2);
 
 int fazer_jogada_melhor(int jogador);
 
+int fazer_jogada_pior(int jogador);
+
 int fim_de_jogo(int pontos_jogador_1, int pontos_jogador_2, int melhor_jogo);
 
 #endif
